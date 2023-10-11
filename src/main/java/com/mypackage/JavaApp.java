f1658b2946515b26694123da189d372bdaf88c60
vinit <vmahiwal@gmail.com>
2023-10-11 07:13:55 +0530

    demoJen1

--- src/main/java/com/mypackage/JavaApp.java
+++ src/main/java/com/mypackage/JavaApp.java
@@ -9,6 +9,8 @@ public class JavaApp {
 
     public static void main(String[] args) {
         System.out.println(MESSAGE);
+        
+        System.out.println(" How are you !!");
     }
 
     public String getMessage() {
